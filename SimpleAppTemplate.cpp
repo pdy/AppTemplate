a52ea9282fbcd407c7d8738a01ed544494e2c96e
Pawel Drzycimski <pdrzycimski@gmail.com>
2020-03-21 22:27:55 +0100

    Minor modifications.

--- SimpleAppTemplate.cpp
+++ SimpleAppTemplate.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
   cmdline::parser arg;
   arg.add("help", 'h', "Print help.");
-  arg.add<std::string>("file", 'f', "File to be hashed.", true);
+//  arg.add<std::string>("file", 'f', "Example file argument.", true);
     
   if(!arg.parse(argc, const_cast<const char* const*>(argv)))
   {
@@ -24,14 +24,16 @@ int main(int argc, char *argv[])
     return 0;
   } 
 
+  /* Example file arg check and get
   if(!arg.exist("file"))
   {
     log << "--file or -f argument is mandatory!\n";
     log << arg.usage();
     return 0;
   }
-
+  
   const std::string file = arg.get<std::string>("file");
+  */
        
   return 0;
 }
